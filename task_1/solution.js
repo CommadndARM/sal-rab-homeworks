aefcc4c6208a427cb5cb536500c6516a000fb26d
Vladimir Alekseev <san-do@mail.ru>
2022-10-30 12:25:14 +0300

    task №1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -13,6 +13,13 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
+    let message = productName;
+
+    message = message + " " + "за" + " " + productPrice + " " +  'теперь в корзине!';
+  
+    console.log (message);
+    
+
 
     // Конец решения задания №1.1.
 
@@ -27,6 +34,10 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+
+    let newValue = oldValue + 1;
+
+    console.log (newValue);
 
     // Конец решения задания №1.2.
 
@@ -43,8 +54,13 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
+    let newSum = oldSum + difference;
+    let newSumText = newSum + " " + '₽';
+
+    console.log (newSumText);
+
+
     // Конец решения задания №1.3.
 
     return newSumText;
 }
-
